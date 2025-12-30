ceb2cf0ee8bc0bc89675fb16f2265b175ca9f715
Deep Agre <deepagre1234@gmail.com>
2025-12-31 01:09:12 +0530

    solving reviews issue

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,15 +1,13 @@
 import { createClient } from '@sanity/client';
 import { createImageUrlBuilder } from '@sanity/image-url';
 
-// This version checks both possible Vite environments
-const token = import.meta.env.VITE_SANITY_WRITE_TOKEN || process.env.VITE_SANITY_WRITE_TOKEN;
-
 export const client = createClient({
   projectId: '688oebb5', 
   dataset: 'production',
   useCdn: false, 
   apiVersion: '2023-05-03',
-  token: token, 
+  // HARDCODE THE TOKEN TEMPORARILY
+  token: 'skb09LBSQIcvTVRFphEsy46KR02fIIeLu7ikkRVdQgLoXHVADXOvy5TzQhOxKVARg2o37e5gbulKaeI9CQkzmmB45G7T1C3vMpX78AW54pbO0UfLQsIz0l703OZvoKuPogSrRXGuGwJQRL3sI9RHMjYPEZlgSHJYnVe39QXkl5W1JveqVxU9', 
 });
 
 const builder = createImageUrlBuilder(client);
